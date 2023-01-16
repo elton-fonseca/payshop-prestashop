37472a012ba26ef21cd048c4f92539c3fd37cd56
Dev7 <dev7@coolsis.pt>
2023-01-16 09:09:54 -0300

    send user agent to payshop gateway

--- includes/sdk/PayshopRestCli.php
+++ includes/sdk/PayshopRestCli.php
@@ -23,6 +23,9 @@ class PayshopRestCli
 
         $connect = curl_init($url);
 
+        $useragent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'webhook';
+        curl_setopt($connect, CURLOPT_USERAGENT, $useragent);
+
         curl_setopt($connect, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($connect, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($connect, CURLOPT_HTTPHEADER, $headers);
