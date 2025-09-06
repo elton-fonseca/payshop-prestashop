2dfe215746b8d9a9f2b0f2a798b79618fdab92a8
Elton Fonseca <elton869@gmail.com>
2025-09-06 13:43:32 -0300

    Fix licence in all files

--- payshop.php
+++ payshop.php
@@ -1,32 +1,31 @@
 <?php
-
-if (!defined('_PS_VERSION_')) {
-    exit;
-}
-/*
- * 2007-2025 PrestaShop.
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
  *
- * NOTICE OF LICENSE*
+ * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * https://opensource.org/licenses/AFL-3.0
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
  *
- * DISCLAIMER*
+ * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
  *
- * @author    Payshop
- * @copyright Copyright (c) Payshop [http://www.payshop.com]
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *  International Registered Trademark & Property of Payshop
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 define('PAYSHOP_VERSION', '2.1.4');
 define('PAYSHOP_ROOT_URL', dirname(__FILE__));
